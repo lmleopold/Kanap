70da9b3d12537257a53c73d68a967d51f2f6a965
lmleopold <louismarie.leopold@gmail.com>
2022-08-29 11:01:58 +0200

    début modification du DOM

--- front/js/index.js
+++ front/js/index.js
@@ -1,12 +1,18 @@
 fetch ("http://localhost:3000/api/products")
-.then (function(res){
+.then ((res) => {
     if (res.ok){
         return res.json ();
     }
 })
-.then (function (data) {
-    console.log (data);
+.then ((data) => {
+    console.log (data);    
 })
-.catch (function (err) {
+.catch ((err) => {
     alert ("erreur :" + err);
 });
+
+const newElt = document.createElement("div");
+newElt.innerHTML = "<h1>salut!</h1>";
+let list = document.getElementById ("items");
+console.log (list);
+list.appendChild(newElt);
