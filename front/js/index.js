74cad7d10186abc366852e1a63665785a2766fc2
lmleopold <louismarie.leopold@gmail.com>
2022-09-05 18:03:32 +0200

    page d'acceuil _ récupération des données produits

--- front/js/index.js
+++ front/js/index.js
@@ -1,18 +1,48 @@
-fetch ("http://localhost:3000/api/products")
-.then ((res) => {
-    if (res.ok){
-        return res.json ();
-    }
-})
-.then ((data) => {
-    console.log (data);    
-})
-.catch ((err) => {
-    alert ("erreur :" + err);
-});
 
-const newElt = document.createElement("div");
-newElt.innerHTML = "<h1>salut!</h1>";
-let list = document.getElementById ("items");
-console.log (list);
-list.appendChild(newElt);
+
+const retrieveProductsData = () =>
+    fetch('http://localhost:3000/api/products')
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .catch((err) => {
+            alert('Argh!\nUne erreur!\n\n' + err);
+        });
+
+async function test() {
+    console.table(await retrieveProductsData());
+}
+test();
+
+const createProductCardImg = product => {
+    const $productImg = document.createElement('img')
+
+    $sensorImg.classList.add('sensor-img')
+
+    $sensorImg.setAttribute('src', `/assets/${sensor.img}`)
+    $sensorImg.setAttribute('alt', `Capteur numéro ${sensor.id}`)
+
+    return $sensorImg
+}
+
+// async function innerText() {
+// const newElt = document.createElement('div');
+// newElt.innerText = await retrieveProductsData();
+// const list = document.getElementById('items');
+// console.log(list);
+// list.appendChild(await newElt);
+// }
+// innerText();
+
+
+
+
+const retrieveSensorsData = () =>
+    fetch('/data/homepage-data.json')
+        .then((res) => res.json())
+        .then((data) => data.facades)
+        .catch((err) => console.log('Oh no', err));
+
+// const main = async();
